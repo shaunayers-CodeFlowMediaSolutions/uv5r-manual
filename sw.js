d2c180f09e0a2511b64056332d4342a35cb628a6
shaunayers-CodeFlowMediaSolutions <shaun_ayers@codeflowmediasolutions.dev>
2026-02-02 22:48:03 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,36 @@
-const CACHE_NAME = 'uv5r-v2';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'uv5r-squad-v3';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/1085/1085814.png'
+];
 
+// Install the Service Worker and Cache Files
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Activate and Clear Old Caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch from Cache first, then Network
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
